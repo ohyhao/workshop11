1c7ca1fbd9e060414f46a506e04508e812c5e45f
ohyhao <ohyonghao@outlook.com>
2022-03-01 20:02:24 +0800

    new

---
+++ src/main/java/sg/edu/nus/iss/workshop11/Random.java
@@ -0,0 +1,21 @@
+package sg.edu.nus.iss.workshop11;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args) {
+        int size = 20;
+
+        ArrayList<Integer> list = new ArrayList<Integer>(size);
+        for(int i = 1; i <= size; i++) {
+            list.add(i);
+        }
+
+        Random rand = new Random();
+        while(list.size() > 0) {
+            int index = rand.nextInt(list.size());
+            System.out.println("Selected: "+list.remove(index));
+        }
+    }
+}
--- src/main/java/sg/edu/nus/iss/workshop11/Workshop11Application.java
+++ src/main/java/sg/edu/nus/iss/workshop11/Workshop11Application.java
@@ -1,13 +1,45 @@
 package sg.edu.nus.iss.workshop11;
 
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.Collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class Workshop11Application {
+	private static final Logger logger = LoggerFactory.getLogger(Workshop11Application.class);
+		
+		private static final String DEFAULT_PORT_NUMBER = "3000";
 
-	public static void main(String[] args) {
-		SpringApplication.run(Workshop11Application.class, args);
-	}
+		public static void main(String[] args) {
+			logger.info("Workshop 11");
+			logger.debug("Workshop 11 - D");
+			String portNumber = null;
+			SpringApplication app = new SpringApplication(Workshop11Application.class);
+			DefaultApplicationArguments appArgs =
+				new DefaultApplicationArguments(args);
 
+			List<String> optVals = appArgs.getOptionValues("port");
+			logger.info("optVals > " + optVals);
+			if(optVals == null || optVals.get(0) == null){
+				portNumber = System.getenv("PORT");
+				if(portNumber == null){
+					portNumber = DEFAULT_PORT_NUMBER;
+				}
+			}else{
+				portNumber = (String)optVals.get(0);
+			}
+
+			if(portNumber != null){
+				app.setDefaultProperties(Collections.singletonMap("server.port", portNumber));
+			}
+
+			app.run(args);
+
+		}
 }
